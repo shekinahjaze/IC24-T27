c879e69b2f384f9b64612f849d14fa02c2e5548d
KimberlyVann <m266528@usna.edu>
2024-02-24 23:31:52 -0500

    Sub Question 1 information

---
+++ participation.py
@@ -0,0 +1,22 @@
+# Sub question 1
+
+# libraries we're using
+
+import pandas as pd
+
+# Looking for how many total participants for all years of the program
+df = pd.read_csv('Data_Level3_GreenTerp - Cleaned.csv')
+print('total participants:' , len(df))
+
+# splitting up the participation based on the years
+yearcounts = df["AY"].value_counts()
+print('2018-2019 participants: 4016')
+print('2019-2020 participants: 3201')
+print('2021-2022 participants: 3205')
+print('2022-2023 participants: 573')
+
+# note: certified means completed 10 things to help the environment and signed up to get a prize
+# https://sites.google.com/umd.edu/greenterp/home holds a lot of information for this program
+
+# seeing when people sign up the most (trends according to holidays/months)
+
