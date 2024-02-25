19bdd5d7ec04fbe7cc5e2a9498b1d40c560c5be7
KimberlyVann <m266528@usna.edu>
2024-02-25 00:21:44 -0500

    Updates to Sub question 1

--- participation.py
+++ participation.py
@@ -3,6 +3,7 @@
 # libraries we're using
 
 import pandas as pd
+import datetime
 
 # Looking for how many total participants for all years of the program
 df = pd.read_csv('Data_Level3_GreenTerp - Cleaned.csv')
@@ -15,8 +16,11 @@ print('2019-2020 participants: 3201')
 print('2021-2022 participants: 3205')
 print('2022-2023 participants: 573')
 
+# seeing when people sign up the most (trends according to holidays/months)
+df["Form Times"] = pd.to_datetime(df["Timestamp"], errors='coerce').dt.tz_localize(None)
+print("months according to their participation\n" + str(df["Form Times"].dt.month.value_counts()))
+
+# finding the ratio of how many people are certified to how many people signed up
 # note: certified means completed 10 things to help the environment and signed up to get a prize
 # https://sites.google.com/umd.edu/greenterp/home holds a lot of information for this program
-
-# seeing when people sign up the most (trends according to holidays/months)
 
