2c3845a517a534139dac55e75b54b66f34374001
m262418 <160156965+m262418@users.noreply.github.com>
2024-02-26 10:41:41 -0500

    Update ratio.sh

--- ratio.sh
+++ ratio.sh
@@ -1,12 +1,19 @@
 %%bash
 
-grep -c "Certified" "/home/m262418/sd212/IC24/Data_Level3_GreenTerp - Cleaned.csv"
-
-# output is 851 
-# ratio is 851 / 10995
+grep "Certified" "/home/m262418/sd212/IC24/Data_Level3_GreenTerp - Cleaned.csv" | grep -c "2023"
+# 51
 
 grep "Certified" "/home/m262418/sd212/IC24/Data_Level3_GreenTerp - Cleaned.csv" | grep -c "2022"
 # 275
 
-grep "Certified" "/home/m262418/sd212/IC24/Data_Level3_GreenTerp - Cleaned.csv" | grep -c "2023"
-# 51
+grep "Certified" "/home/m262418/sd212/IC24/Data_Level3_GreenTerp - Cleaned.csv" | grep -c "2021"
+# 224
+
+grep "Certified" "/home/m262418/sd212/IC24/Data_Level3_GreenTerp - Cleaned.csv" | grep -c "2020"
+# 576
+
+grep "Certified" "/home/m262418/sd212/IC24/Data_Level3_GreenTerp - Cleaned.csv" | grep -c "2019"
+# 576
+
+grep "Certified" "/home/m262418/sd212/IC24/Data_Level3_GreenTerp - Cleaned.csv" | grep -c "2018"
+# 3
